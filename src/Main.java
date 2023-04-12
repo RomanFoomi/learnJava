88baf2b6a9e9906ea39cf5d830ccc37c4551ea65
RoomanFoomi <rewev@gmail.com>
2023-04-12 16:17:06 +0300

    Variables is boring)

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    /*  First lessons variables
+        {
+            System.out.println("Enter two number");
+            System.out.println("________________");
+
+            double a, b;
+            Scanner in = new Scanner(System.in);
+
+            System.out.println("Enter A number: ");
+            a = in.nextDouble();
+
+            System.out.println("Enter B number: ");
+            b = in.nextDouble();
+
+            System.out.println("A + B = " + (a + b));
+        }
+    */
+
+        {
+
+        }
+    }
+}
